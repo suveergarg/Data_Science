e20c01fbc096296660cfe51ab2622f04ed447244
Suveer <gargsuveer@gmail.com>
2018-02-10 00:30:49 +0530

    Create bootstrap_pairs.py
    Draws bootstrapped pairs from x and y and return replicates using func

---
+++ bootstrap_pairs.py
@@ -0,0 +1,17 @@
+def draw_bs_pairs(x, y,func, size=1):
+    """Draws bs Pairs"""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates
+    bs_replicates = np.empty(size)
+
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds,len(inds))
+        bs_x, bs_y = x[bs_inds],y[bs_inds]
+        bs_replicates[i]=func(bs_x,bs_y)
+
+    return bs_replicates
